b2346ba7a1a389d78b7864fe1975a4a840af6fa2
Nikita Voropaev <bambook97@gmail.com>
2024-03-05 01:19:55 +0600

    init

--- src/Pages/admin/Admin.tsx
+++ src/Pages/admin/Admin.tsx
@@ -1,7 +1,9 @@
 import { AdminForm } from "../../Components/AdminForm/AdminForm.tsx";
 import style from "./admin.module.scss";
+import { useNavigate } from "react-router-dom";
 
 const Admin = () => {
+  const navigate = useNavigate();
   return (
     <div className={style.wrapper}>
       <h1>Admin Panel</h1>
@@ -15,6 +17,7 @@ const Admin = () => {
       <AdminForm initValue={""} />
       <AdminForm initValue={""} />
       <AdminForm initValue={"Connecting..."} />
+      <button onClick={() => navigate("/")}>Go to preview</button>
     </div>
   );
 };
